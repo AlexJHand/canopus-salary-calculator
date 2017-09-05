78a8f677b88e7d17a40a7f4ddeacd535563f05e6
Christopher Black <chris@primeacademy.io>
2017-09-05 09:42:54 -0500

    Add id, job title, salary and last name

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 - [x] Set up index.html and source in JS files
 - [x] Create a input field for the first name
 - [x] Append that name to the DOM in a HTML table
-- [ ] Add id, job title, salary and last name
+- [x] Add id, job title, salary and last name
 - [ ] Salary total calculation
 - [ ] Refactor the code into functions
 - [ ] Ability to remove a user
--- script.js
+++ script.js
@@ -7,9 +7,26 @@ $(document).ready(function(){
         var $row = $('<tr></tr>');
         $row.append('<td>' + firstName + '</td>');
         // <tr><td>Name</td></tr>
-        $('#employeeData').append($row); 
+
+        // First, last, job title, id, salary
+        var lastName = $('#lastName').val();
+        $row.append('<td>' + lastName + '</td>');
+
+        var jobTitle = $('#jobTitle').val();
+        $row.append('<td>' + jobTitle + '</td>');
+
+        var id = $('#employeeId').val();
+        $row.append('<td>' + id + '</td>');
+
+        var salary = $('#salary').val();
+        $row.append('<td>' + salary + '</td>');
+
+        // Put the row on the DOM
+        $('#employeeData').append($row);
         // Also written as...
         //$('#employeeData').append('<tr><td>' + firstName + '</td></tr>');
 
+        // Clear all values
+        $('.salaryInput').val('');
     });
 });
