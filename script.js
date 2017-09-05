5fb2041a90b5ed20d845b051fafbaee36651b83f
Christopher Black <chris@primeacademy.io>
2017-09-05 08:57:13 -0500

    Create a input field for the first name

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     console.log('jquery sourced.');
+
+    $('#submitButton').on('click', function(){
+        var firstName = $('#firstName').val();
+        console.log(firstName);
+    });
 });
